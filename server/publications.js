b072c21f4b59ac5acf4242cd47b0def106429f2f
James Lefrère <jlefrere@gmail.com>
2014-05-02 22:50:36 +0100

    Added StreamQueue

--- server/publications.js
+++ server/publications.js
@@ -2,3 +2,8 @@ Meteor.publish('userPresence', function () {
 	var filter = { userId: { $exists: true }};
 	return Presences.find(filter, {fields: {state: true, userId: true}});
 });
+
+
+Meteor.publish('streamQueue', function () {
+	return StreamQueue.find({}, {limit: 10});
+});
--- server/security.js
+++ server/security.js
@@ -1,3 +1,15 @@
+StreamQueue.deny({
+	insert: function() {
+		return true;
+	},
+	update: function() {
+		return true;
+	},
+	remove: function() {
+		return true;
+	}
+});
+
 VideoStream.permissions.write(function (eventName) {
 	return false;
 });
