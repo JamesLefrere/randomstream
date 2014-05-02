5e8cea2163520f495d520f14cf163f462076b3f1
James Lefrère <jlefrere@gmail.com>
2014-05-02 15:08:33 +0100

    Accounts username/password only

--- client/client.js
+++ client/client.js
@@ -2,4 +2,6 @@ VideoStream.on('newStream', function (get) {
 	var stream = get.data.results[0];
 	stream.iframe = '<iframe width="720" src="http://www.ustream.tv/embed/'+stream.id+'?v=3&wmode=direct&autoplay=true" scrolling="no" frameborder="0" style="border: 0px none transparent;"></iframe>';
 	Session.set('currentStream', stream);
+});Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_ONLY'
 });
