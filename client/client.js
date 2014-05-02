b08e1617802d2176d045818c66f82d3ff2b4ab59
James Lefrère <jlefrere@gmail.com>
2014-05-02 22:49:42 +0100

    Added event hooks to enable textarea post login

--- client/client.js
+++ client/client.js
@@ -2,8 +2,17 @@ VideoStream.on('newStream', function (get) {
 	var stream = get.data.results[0];
 	stream.iframe = '<iframe src="http://www.ustream.tv/embed/'+stream.id+'?v=3&wmode=direct&autoplay=true" scrolling="no" frameborder="0"></iframe>';
 	Session.set('currentStream', stream);
+Meteor.startup(function () {
+	Hooks.init();
+});
 });
 
 Accounts.ui.config({
 	passwordSignupFields: 'USERNAME_ONLY'
 });
+
+Hooks.onLoggedIn = function () {
+	Meteor.setTimeout(function () {
+		$('#chat-text').removeAttr('disabled').focus();
+	}, 1500);
+};
