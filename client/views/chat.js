6bf7109f9cd454103f9a75872bae798db64466c1
James Lefrère <jlefrere@gmail.com>
2014-05-02 17:10:36 +0100

    Trim local Chat collection to 100 by removing based on index – minimongo doesn't support capped collections

--- client/views/chat.js
+++ client/views/chat.js
@@ -1,12 +1,15 @@
 Chat = new Meteor.Collection(null);
 
+var chatIndex = 0;
+var chatLimit = 100;
+
 ChatStream.on('chat', function (data) {
 	Chat.insert(data);
 });
 
 Template.chat.helpers({
 	messages: function () {
-		return Chat.find();
+		return Chat.find({}, {sort: {time: -1}});
 	}
 });
 
@@ -14,10 +17,14 @@ Template.chat.events({
 	'submit #chat-form': function(e) {
 		e.preventDefault();
 		var data = {
+			time: (new Date()).getTime(),
 			username: Meteor.user().username,
-			message: $('#chat-text').val()
+			message: $('#chat-text').val(),
+			index: chatIndex
 		};
 		Chat.insert(data);
+		chatIndex++;
+		Chat.remove({index: {$lt: chatIndex - chatLimit}});
 		ChatStream.emit('chat', data);
 		$('#chat-text').val('').focus();
 	}
