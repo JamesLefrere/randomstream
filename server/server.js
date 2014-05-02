a9a74470910bc5441eac2b807dc70ff93f00478e
James Lefrère <jlefrere@gmail.com>
2014-05-02 22:55:57 +0100

    Added methods for youtube, twitch, ustream, for specific URLs and random videos. Not implemented yet.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-var Schedule = Meteor.require('node-schedule');
-var Fiber = Npm.require('fibers');
 var timer;
 var waitTime = 60000;
 var streamQueueIndex = 0;
@@ -20,15 +18,45 @@ Meteor.startup(function () {
 });
 
 Meteor.methods({
-  getStream: function () {
 	queueStream: function (data) {
 		StreamQueue.insert({url: data.url, username: data.username, time: data.time, index: data.index});
 		streamQueueIndex++;
 		StreamQueue.remove({index: {$lt: streamQueueIndex - streamQueueLimit}});
 	},
+  getUStream: function (url) {
 	  this.unblock();
-	  return Meteor.http.call('GET', 'http://api.ustream.tv/json/stream/all/getRandom?key=F529A1377B4F8A45B55EC457A2B0648B&params=autoplay:true;height:100;width:100');
-  }
+	  if (url !== null) {
+		  // parse ustream url, return api call
+	  } else {
+		  return Meteor.http.call('GET', 'http://api.ustream.tv/json/stream/all/getRandom?key=F529A1377B4F8A45B55EC457A2B0648B&params=autoplay:true;height:100;width:100');
+	  }
+  },
+  getTwitch: function (url) {
+	  this.unblock();
+	  if (url !== 'undefined') {
+		  // parse twitch url, return api call
+	  } else {
+		  // return api call for random twitch video
+	  }
+  },
+	getYoutube: function (url) {
+		this.unblock();
+		if (url !== 'undefined') {
+			// parse youtube url, return api call
+		} else {
+			// return api call for random youtube video
+		}
+	},
+	randomStream: function () {
+		var randy = Math.random();
+		if (randy < 0.33) {
+			VideoStream.emit('newStream', Meteor.call('getUStream', null));
+		} else if (randy < 0.66) {
+			VideoStream.emit('newStream', Meteor.call('getTwitch', null));
+		} else {
+			VideoStream.emit('newStream', Meteor.call('getYoutube', null));
+		}
+	},
 	nextStream: function () {
 		console.log('next! timer reset');
 		clearTimer();
@@ -40,13 +68,6 @@ Meteor.methods({
 	}
 });
 
-var rule = new Schedule.RecurrenceRule();
-rule.second = [0, 30];
-
-var job = Schedule.scheduleJob(rule, function () {
-	Fiber(function () {
-		VideoStream.emit('newStream', Meteor.call('getStream'));
-	}).run();
 ChatStream.on('chat', function (data) {
 	if (data.message.substring(0, 1) === '/') {
 		switch (data.message) {
