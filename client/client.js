08a197e20b265c54f1c21c0e79b9a17a9ed702e8
James Lefrère <jlefrere@gmail.com>
2014-05-02 22:49:56 +0100

    Disabled newStream event in client for the moment

--- client/client.js
+++ client/client.js
@@ -1,10 +1,14 @@
-VideoStream.on('newStream', function (get) {
-	var stream = get.data.results[0];
-	stream.iframe = '<iframe src="http://www.ustream.tv/embed/'+stream.id+'?v=3&wmode=direct&autoplay=true" scrolling="no" frameborder="0"></iframe>';
-	Session.set('currentStream', stream);
 Meteor.startup(function () {
 	Hooks.init();
 });
+
+VideoStream.on('newStream', function (data) {
+	console.log('newStream');
+	console.log(data);
+	// need to do this on the server
+	//	var stream = data.data.results[0];
+	//	stream.iframe = '<iframe src="http://www.ustream.tv/embed/'+stream.id+'?v=3&wmode=direct&autoplay=true" scrolling="no" frameborder="0"></iframe>';
+	//	Session.set('currentStream', stream);
 });
 
 Accounts.ui.config({
