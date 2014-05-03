335a1163cfa0a121028b10a96ce698761c5db143
James Lefrère <jlefrere@gmail.com>
2014-05-03 15:15:14 +0100

    Removed randomness – not really useful anymore

--- server/server.js
+++ server/server.js
@@ -31,34 +31,18 @@ Meteor.methods({
 	  this.unblock();
 	  if (url !== null) {
 		  // parse ustream url, return api call
-	  } else {
-		  return Meteor.http.call('GET', 'http://api.ustream.tv/json/stream/all/getRandom?key=F529A1377B4F8A45B55EC457A2B0648B&params=autoplay:true;height:100;width:100');
 	  }
   },
   getTwitch: function (url) {
 	  this.unblock();
 	  if (url !== 'undefined') {
 		  // parse twitch url, return api call
-	  } else {
-		  // return api call for random twitch video
 	  }
   },
 	getYoutube: function (url) {
 		this.unblock();
 		if (url !== 'undefined') {
 			// parse youtube url, return api call
-		} else {
-			// return api call for random youtube video
-		}
-	},
-	randomNewItem: function () {
-		var randy = Math.random();
-		if (randy < 0.33) {
-			ItemStream.emit('newStream', Meteor.call('getUStream', null));
-		} else if (randy < 0.66) {
-			ItemStream.emit('newStream', Meteor.call('getTwitch', null));
-		} else {
-			ItemStream.emit('newStream', Meteor.call('getYoutube', null));
 		}
 	},
 	nextItem: function () {
