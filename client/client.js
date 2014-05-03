bf1c51732ac46fd8dde8b8194983913b718a31b8
James Lefrère <jlefrere@gmail.com>
2014-05-03 22:58:24 +0100

    Youtube API integration for client

--- client/client.js
+++ client/client.js
@@ -1,11 +1,38 @@
 Meteor.startup(function () {
 	Hooks.init();
+	Session.set('YTApiReady', false);
 });
 
-ItemStream.on('newItem', function (data) {
-	console.log('newItem');
-	console.log(data);
-	Session.set('currentItem', data);
+onYouTubeIframeAPIReady = function () {
+	Session.set('YTApiReady', true);
+};
+
+VideoStream.on('newItem', function (data) {
+	Session.set('youtubeId', data.youtubeId);
+
+	if (Session.equals('YTApiReady', false))
+		return;
+
+	var interval = Meteor.setInterval(function () {
+		if (!document.getElementById('video'))
+			return;
+
+		var playerDiv = document.createElement('div'),
+			youtubeId = Session.get('youtubeId');
+
+		playerDiv.id = 'video';
+		document.getElementById('video-wrapper').innerHTML = '';
+		document.getElementById('video-wrapper').appendChild(playerDiv);
+
+		player = null;
+		player = new YT.Player('video', {
+			videoId: youtubeId
+			//			events: {
+			//				'onReady': onPlayerReady
+			//			}
+		});
+		Meteor.clearInterval(interval);
+	}, 1000);
 });
 
 Accounts.ui.config({
