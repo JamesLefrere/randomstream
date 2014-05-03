14c7bc10001d8a8bee1943f2da8941e2f20f46a5
James Lefrère <jlefrere@gmail.com>
2014-05-03 21:19:32 +0100

    Item for youtube embed

--- client/client.js
+++ client/client.js
@@ -5,10 +5,7 @@ Meteor.startup(function () {
 ItemStream.on('newItem', function (data) {
 	console.log('newItem');
 	console.log(data);
-	// need to do this on the server
-	//	var stream = data.data.results[0];
-	//	stream.iframe = '<iframe src="http://www.ustream.tv/embed/'+stream.id+'?v=3&wmode=direct&autoplay=true" scrolling="no" frameborder="0"></iframe>';
-	//	Session.set('currentStream', stream);
+	Session.set('currentItem', data);
 });
 
 Accounts.ui.config({
