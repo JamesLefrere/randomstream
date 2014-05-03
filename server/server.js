0102ee83dd3a5db1c16109f8ea20fc6ed221309c
James Lefrère <jlefrere@gmail.com>
2014-05-03 16:11:52 +0100

    Process URLs to update title

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+Fiber = Npm.require('fibers');
+
 var timer;
 var waitTime = 60000;
 var streamQueueIndex = 0;
@@ -13,13 +15,36 @@ var clearTimer = function () {
 	Meteor.clearInterval(timer);
 };
 
+var getTitle = function (item, data) {
+	var itemId = item;
+	var url = data.url;
+	Meteor.http.get(url, function (err, res) {
+		if (!err) {
+			var titleRegex = new RegExp(/<title[^>]*>([^<]+)<\/title>/);
+			var matchedTitle = res.content.match(titleRegex) !== null ? res.content.match(titleRegex)[1] : false;
+		} else console.log(err);
+		Fiber(function () {
+			if (matchedTitle)
+				ItemQueue.update(itemId, {$set: {title: matchedTitle}});
+		}).run();
+	});
+};
+
 Meteor.startup(function () {
 	setTimer();
 });
 
 Meteor.methods({
 	queueItem: function (data) {
-		ItemQueue.insert({url: data.url, username: data.username, time: data.time, index: data.index, score: 0, upvoted: []});
+		if (!/^(http|https):\/\//i.test(data.url))
+			data.url = 'http://' + data.url;
+		var item = ItemQueue.insert({title: data.url, url: data.url, username: data.username, time: data.time, index: data.index, score: 0, upvoted: []});
+		var parseItem = Meteor.bindEnvironment(function () {
+			getTitle(item, data);
+		}, function (err) {
+			throw(err);
+		});
+		parseItem();
 		streamQueueIndex++;
 		ItemQueue.remove({index: {$lt: streamQueueIndex - streamQueueLimit}});
 	},
